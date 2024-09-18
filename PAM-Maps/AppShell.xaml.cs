dc8b3dbf108e27771e05bf7e49876a5b8f272226
end <endy.kaori.yara@gmail.com>
2024-09-18 11:32:35 -0300

    Lua

---
+++ PAM-Maps/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PAM_Maps
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PAM-Maps/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace PAM_Maps
+
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+
+            InitializeComponent();
+            //inicialização do mapa
+            var location = new Location(-23.51955620023332, -46.595764769894195);
+            var mapSpan = new MapSpan(location, 0.01, 0.01);
+            map.MoveToRegion(mapSpan);
+        }
+
+    }
+
+}
